13c8fb444d74cb08964009a6b885cf67bb52a122
thebeninator <xccr123@gmail.com>
2026-02-05 04:06:51 +0000

    live feed

--- bensecuritysystems.py
+++ bensecuritysystems.py
@@ -2,24 +2,33 @@ import uvicorn
 import cv2
 import logging
 import datetime
+import asyncio
 from fastapi import FastAPI, Response
+from fastapi.responses import StreamingResponse
+from contextlib import asynccontextmanager
 
-app = FastAPI()
-webcam = cv2.VideoCapture(0)
+logging.basicConfig(
+  format="%(asctime)s.%(msecs)03dZ %(levelname)s:%(name)s:%(message)s",
+  datefmt="%Y-%m-%dT%H:%M:%S",
+  level=logging.INFO
+)
+logging.getLogger("uvicorn.access").setLevel(logging.WARNING)
+logging.getLogger("uvicorn.error").setLevel(logging.WARNING)
 
-logging.basicConfig(level=logging.INFO)
+@asynccontextmanager
+async def lifespan(app):
+  yield
+  webcam.release()
 
-async def frame_generator():
-  pass
+app = FastAPI(lifespan=lifespan)
 
-@app.get("/feed")
-async def feed():  
-  if not webcam.isOpened():
-    return Response()
+def get_frame_bytes():
+  if not webcam or not webcam.isOpened():
+    return
 
-  ret, frame = webcam.read()
+  did_read_frame, frame = webcam.read()
 
-  if not ret:
+  if not did_read_frame:
     logging.error("failed to read frame from webcam")
 
   cv2.putText(
@@ -30,18 +39,39 @@ async def feed():
     thickness=1, 
     color=(255, 255, 255),
     org=(10, 20)
-  )  
+  )
 
-  ret, jpeg = cv2.imencode(".jpg", frame)
+  encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
+  did_encode_frame, jpeg = cv2.imencode(".jpg", frame, encode_param)
 
-  if not ret:
-    logging.error("failed to encode frame")
+  if not did_encode_frame:
+    logging.error("failed to encode frame")    
 
-  frame_bytes = jpeg.tobytes()
+  return jpeg.tobytes()
 
-  return Response(content=frame_bytes, media_type="image/jpg")
+async def frame_generator():
+  while True:
+    frame = get_frame_bytes()
+    if frame:
+      yield (
+        b'--frame\r\n'
+        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+      )
+
+    await asyncio.sleep(0.15)
+
+@app.get("/feed")
+async def feed(): 
+  if not webcam or not webcam.isOpened():
+    return Response("NO FEED - FAILED TO OPEN WEBCAM")
+
+  return StreamingResponse(
+    frame_generator(),
+    media_type='multipart/x-mixed-replace; boundary=frame'
+  )
+
+if __name__ == "bensecuritysystems":
+  webcam = cv2.VideoCapture(0)
 
 if __name__ == "__main__":
-  webcam = cv2.VideoCapture(0)
   uvicorn.run("bensecuritysystems:app", host="0.0.0.0", port=9999)
-
